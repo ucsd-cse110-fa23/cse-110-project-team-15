a50aeba6f76010c6a2a5e8f1f1701085588261d1
SRaswan <shauryaraswan@gmail.com>
2023-11-02 14:24:59 -0700

    #16 #17 #18 adding gradle workflow and tests

---
+++ src/tests/Iter1.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.Test;
+
+import java.util.Objects;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class Iter1 {
+
+
+    @Test
+    public void testStudentEquals() {
+
+    }
+
+    @Test
+    public void testStudentCompareTo() {
+
+    }
+}
