28f7caabe35dc15e8e04a5fca42896419788a82d
SRaswan <shauryaraswan@gmail.com>
2023-11-04 00:04:55 -0700

    #16 Creating story test intitalization and setup for UI by creating javafx thread before each test

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,7 +1,12 @@
-// package test;
+
+//import RecipeManagement.RecipePopup.DetailsPopup;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,21 +16,49 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 // import org.junit.Test;
 // import static org.junit.Assert;
 
-public class Test1 {
+public class Test1 extends Main {
+    private static Application app;
+
+    @BeforeAll
+    public static void initializeJavaFX() throws Exception {
+        if (Platform.isFxApplicationThread()) {
+            app = new Main();
+        } else {
+            Platform.startup(() -> {
+                app = new Main();
+            });
+        }
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        int x = 1;
+    }
 
     @Test
-    public void testStudentCompareTo() {
+    public void test1() throws Exception{
+        
         assertEquals("hi", "hi");
     }
 
     @Test
-    public void wrongTest() {
-        assertEquals("hi", "hi");
-        assertEquals("hi", "he");
+    public void recipeCreate() throws Exception{
+
+        Recipe rec = new Recipe();
+        String name = "Chicken salad";
+        String mealType = "Lunch";
+        String ingredient = "Hello";
+        String instruction = "hi";
+
+        rec.getName().setText(name);
+        rec.getMealType().setText(mealType);
+        rec.getIngredient().setText(ingredient);
+        rec.getInstruction().setText(instruction);
+
+        assertEquals(rec.getName().getText(), name);
+        assertEquals(rec.getMealType().getText(), mealType);
+        assertEquals(rec.getIngredient().getText(), ingredient);
+        assertEquals(rec.getInstruction().getText(), instruction);
     }
-    @Test
-    public void test2() {
-        assertEquals("hi", "hi");
-        assertEquals("hi", "he");
-    }
+
 }
