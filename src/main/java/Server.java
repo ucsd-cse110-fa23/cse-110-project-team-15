3e274ed4b66771a7f5f1d8a4594cc4a7b17eca57
SRaswan <shauryaraswan@gmail.com>
2023-11-12 15:23:37 -0800

    Added request handler and server for controller

---
+++ src/main/java/Server.java
@@ -0,0 +1,43 @@
+import com.sun.net.httpserver.*;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.*;
+
+
+public class Server {
+
+
+  // initialize server port and hostname
+  private static final int SERVER_PORT = 8100;
+  private static final String SERVER_HOSTNAME = "localhost";
+
+
+  public static void main(String[] args) throws IOException {
+    ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+    
+
+    // create a map to store data
+    Map<String, String> data = new HashMap<>();
+
+
+    // create a server
+    HttpServer server = HttpServer.create(
+      new InetSocketAddress(SERVER_HOSTNAME, SERVER_PORT),
+      0
+    );
+
+    // TODO: create the context
+    server.createContext("/", new RequestHandler(data));
+    // TODO: set the executor
+    server.setExecutor(threadPoolExecutor);
+    // TODO: start the server
+    server.start();
+
+    
+
+    System.out.println("Server started on port " + SERVER_PORT);
+
+  }
+}
