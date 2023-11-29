f22cd8ce1ebb9dd7b8459e744ea749f6e09ae3da
gjquach <114312610+gjquach@users.noreply.github.com>
2023-11-28 20:15:32 -0800

    #76

--- src/main/java/server/CreateAccount.java
+++ src/main/java/server/CreateAccount.java
@@ -7,26 +7,23 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
-
 import static java.util.Arrays.asList;
 
-public class Create {
+public class CreateAccount {
 
     public static void createAccount(String username, String password) {
-        String uri =
-        "mongodb+srv://aditijain:cse110project@cluster0.yu0exzy.mongodb.net/?retryWrites=true&w=majority";
+        String uri = "mongodb+srv://aditijain:cse110project@cluster0.yu0exzy.mongodb.net/?retryWrites=true&w=majority";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
 
             MongoDatabase PantryPalDB = mongoClient.getDatabase("PantryPal");
             MongoCollection<Document> accountsCollection = PantryPalDB.getCollection("accounts");
 
             Document user = new Document("_id", new ObjectId())
-                .append("username", username).append("password", password);
+                    .append("username", username).append("password", password);
             accountsCollection.insertOne(user);
 
             System.out.println("Account created for: " + username);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
