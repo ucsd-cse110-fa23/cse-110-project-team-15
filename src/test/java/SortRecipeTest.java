e9d703b9094209aa42c3fffea277161e186d74e6
gjquach <114312610+gjquach@users.noreply.github.com>
2023-12-06 22:23:17 -0800

    merge

--- src/test/java/SortRecipeTest.java
+++ src/test/java/SortRecipeTest.java
@@ -89,4 +89,61 @@ public class SortRecipeTest {
             assertEquals(newestTime, recipe2.getRecipeIdAsDateTime());
         });
     }
+
+    @Test
+    public void testSortRecipesLexAscending() {
+        Platform.runLater(() -> {
+            Recipe recipe1 = new Recipe(new AppFrame());
+            Recipe recipe2 = new Recipe(new AppFrame());
+            Recipe recipe3 = new Recipe(new AppFrame());
+
+            recipe1.getName().setText("Cereal");
+            recipe2.getName().setText("Apple Pie");
+            recipe3.getName().setText("Banana Smoothie");
+
+            recipeList.addRecipe(recipe1);
+            recipeList.addRecipe(recipe2);
+            recipeList.addRecipe(recipe3);
+
+            recipeList.sortOptions.getCheckModel().check("A-Z");
+            recipeList.sortRecipesLex();
+
+            assertEquals(recipe1.getNameText(), recipeList.getRecipes().get(0).getNameText());
+            assertEquals(recipe2.getNameText(), recipeList.getRecipes().get(1).getNameText());
+            assertEquals(recipe3.getNameText(), recipeList.getRecipes().get(2).getNameText());
+        });
+    }
+
+    @Test
+    public void testSortRecipesLexDescending() {
+        Platform.runLater(() -> {
+            Recipe recipe1 = new Recipe(new AppFrame());
+            Recipe recipe2 = new Recipe(new AppFrame());
+            Recipe recipe3 = new Recipe(new AppFrame());
+
+            recipe1.getName().setText("Cereal");
+            recipe2.getName().setText("Apple Pie");
+            recipe3.getName().setText("Banana Smoothie");
+
+            recipeList.addRecipe(recipe1);
+            recipeList.addRecipe(recipe2);
+            recipeList.addRecipe(recipe3);
+
+            recipeList.sortOptions.getCheckModel().check("Z-A");
+            recipeList.sortRecipesLex();
+
+            assertEquals(recipe3.getNameText(), recipeList.getRecipes().get(0).getNameText());
+            assertEquals(recipe2.getNameText(), recipeList.getRecipes().get(1).getNameText());
+            assertEquals(recipe1.getNameText(), recipeList.getRecipes().get(2).getNameText());
+        });
+    }
+
+    @Test
+    public void sortE2E() {
+        testNoSort();
+        testSortByNewest();
+        testSortByOldest();
+        testSortRecipesLexAscending();
+        testSortRecipesLexDescending();
+    }
 }
